9335cf490b440e4250950b7b880ac4a261b678e0
Luis Urzúa <lurzua@softup.cl>
2023-11-09 11:42:39 -0300

    Primer commit

---
+++ src/presentation/server.ts
@@ -0,0 +1,36 @@
+import express from 'express'
+import path from 'path';
+
+interface Options {
+    port: number;
+    public_path?: string;
+}
+
+export class Server {
+
+    private app = express();
+    private readonly port :number;
+    private readonly publicPath: string;
+
+    constructor(options:Options){
+        const {port, public_path= 'public'} = options;
+        this.port = port;
+        this.publicPath = public_path;
+    }
+
+    async start(){
+
+        //Public 
+        this.app.use(express.static(this.publicPath));
+        this.app.get('*', (req,res )=>{
+            const indexPath = path.join(__dirname + `../../../${this.publicPath}/index.html`);
+            res.sendFile(indexPath);
+        });
+        
+        this.app.listen(3000, ()=>{
+            console.log(`Servidor corriendo en el puerto ${this.port}`);
+        });
+
+    }
+
+}
